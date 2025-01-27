ddcd9ac61384e5458e04c1b0bbeec1b86b5d5a30
Reda Oubih <oubihreda@gmail.com>
2025-01-27 09:29:34 -0600

    my Second commit

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files (HTML, CSS, JS, images)
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to serve the main HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'Project2.html'));
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
+ 
