15b355b94acea602d72a8c281469be4f4676e5e1
Reda Oubih <oubihreda@gmail.com>
2025-01-27 11:15:55 -0600

    server commit 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8000;
+const PORT = 1000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
