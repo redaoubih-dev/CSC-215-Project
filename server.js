309fec07cb69ab03137e4f937888593075f8cddb
Reda Oubih <oubihreda@gmail.com>
2025-01-27 12:52:29 -0600

    final commit1

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-// server.js
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = 1000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to serve the main HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Project2.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
